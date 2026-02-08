fb85c94d69176f2dfd499d6cfc71f1f5f85e91e2
Ty Busby <tybusby@gmail.com>
2026-02-08 03:21:58 -0500

    simplify

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,10 +3,11 @@ import { Middleware, Unauthorized, Worker } from "@adonix.org/cloud-spark";
 class Authorization implements Middleware {
     handle(worker: Worker, next: () => Promise<Response>): Promise<Response> {
         const auth = worker.request.headers.get("Authorization");
-        if (!auth || auth !== `Bearer ${worker.env.LIVEIMAGE_ADMIN_KEY}`) {
-            return new Unauthorized().response();
+        if (auth === `Bearer ${worker.env.LIVEIMAGE_ADMIN_KEY}`) {
+            return next();
         }
-        return next();
+
+        return new Unauthorized().response();
     }
 }
 
